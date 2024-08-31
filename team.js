be1c78798768fa06db49d59c00085c75215724a5
ratchanona65 <ratchanona65@nu.ac.th>
2024-08-31 22:28:32 +0700

    commit add Team page

---
+++ team.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const items = document.querySelectorAll('.personal, .emp-item');
+    items.forEach(item => {
+        observer.observe(item);
+    });
+
+});
+
